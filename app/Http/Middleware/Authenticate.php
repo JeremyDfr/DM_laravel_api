b558761968dbb94a536034e50782b1e2b09af6cc
JeremyDfr <dufroyjeremy@gmail.com>
2022-11-29 16:48:58 +0100

    Change middleware auth & add all routes auth + CRUD

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,22 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param Request $request
+     * @return JsonResponse
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): JsonResponse
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return response()->json([
+            'status' => 'NOT OK',
+            'message' => "Vous n'êtes pas connecté !"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ProduitController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/inscription', [AuthController::class, 'register']);
+Route::post('/connexion', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/deconnexion', [AuthController::class, 'disconnect']);
+
+    // Liste de course
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::post('/course', [CourseController::class, 'create']);
+    Route::get('/course/{course}', [CourseController::class, 'read']);
+    Route::put('/course/{course}', [CourseController::class, 'update']);
+    Route::delete('/course/{course}', [CourseController::class, 'delete']);
+
+    // Produit
+    Route::get('/produits/{course}', [ProduitController::class, 'index']);
+    Route::post('/produit', [ProduitController::class, 'create']);
+    Route::put('/produit/{produit}', [ProduitController::class, 'update']);
+    Route::delete('/produit/{produit}', [ProduitController::class, 'delete']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
